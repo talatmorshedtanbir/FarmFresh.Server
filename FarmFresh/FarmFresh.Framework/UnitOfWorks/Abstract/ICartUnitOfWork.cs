ea862274fe8776cd73acdb2bcdec53d314ddf4cd
Talat Tanbir <hossaintanbir0@gmail.com>
2022-11-13 01:48:27 +0600

    Cart Repository and UnitOfWork Layer

---
+++ FarmFresh/FarmFresh.Framework/UnitOfWorks/Abstract/ICartUnitOfWork.cs
@@ -0,0 +1,10 @@
+﻿using FarmFresh.Data;
+using FarmFresh.Framework.Repositories.Abstract;
+
+namespace FarmFresh.Framework.UnitOfWorks.Abstract
+{
+    public interface ICartUnitOfWork : IUnitOfWork
+    {
+        public ICartRepository CartRepository { get; set; }
+    }
+}
