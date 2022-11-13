2eb702053088cca2cfb58ac4eeac83749c711883
Talat Tanbir <hossaintanbir0@gmail.com>
2022-11-13 18:14:22 +0600

    PASSED -> AddAsync_CategoryAlreadyExists_ShouldThrowDuplicationException()

--- FarmFresh/Tests/FarmFresh.Framework.Tests.Unit/Services/Concrete/CategoryServiceTests.AddAsync.cs
+++ FarmFresh/Tests/FarmFresh.Framework.Tests.Unit/Services/Concrete/CategoryServiceTests.AddAsync.cs
@@ -1,4 +1,5 @@
-﻿using FarmFresh.Framework.Entities.Categories;
+﻿using FarmFresh.Common.Exceptions;
+using FarmFresh.Framework.Entities.Categories;
 using FarmFresh.Framework.Models.Requests;
 using Force.DeepCloner;
 using Moq;
@@ -13,8 +14,6 @@ namespace FarmFresh.Framework.Tests.Unit.Services.Concrete
         public async Task AddAsync_NoErrorOccurs_ShouldAddCategory()
         {
             // given
-            int randomCategoryId = GetRandomNumber();
-            int inputCategoryId = randomCategoryId;
             Category randomCategory = GetRandomCategory();
             Category repositoryCategory = randomCategory.DeepClone();
             AddCategoryRequest inputCategory = CreateCategoryAddRequest(randomCategory);
@@ -45,6 +44,56 @@ namespace FarmFresh.Framework.Tests.Unit.Services.Concrete
             _categoryUnitOfWorkMock.Verify(repository =>
                 repository.SaveChangesAsync(),
                     Times.Once);
+
+            _categoryRepositoryMock.VerifyNoOtherCalls();
+            _categoryUnitOfWorkMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public async Task AddAsync_RequestObjectIsNull_ShouldThrowNullRequestException()
+        {
+            // given
+            AddCategoryRequest inputCategory = null;
+
+            _categoryUnitOfWorkMock.Setup(x => x.CategoryRepository).Returns(_categoryRepositoryMock.Object);
+
+            // when
+            Task addtCategoryByIdTask =
+                _categoryService.AddAsync(inputCategory);
+
+            // then
+            await Assert.ThrowsAsync<NullRequestException>(() =>
+                addtCategoryByIdTask);
+
+            _categoryRepositoryMock.VerifyNoOtherCalls();
+            _categoryUnitOfWorkMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public async Task AddAsync_CategoryAlreadyExists_ShouldThrowDuplicationException()
+        {
+            // given
+            Category randomCategory = GetRandomCategory();
+            Category repositoryCategory = randomCategory.DeepClone();
+            AddCategoryRequest inputCategory = CreateCategoryAddRequest(randomCategory);
+
+            _categoryUnitOfWorkMock.Setup(x => x.CategoryRepository).Returns(_categoryRepositoryMock.Object);
+
+            _categoryRepositoryMock.Setup(repository =>
+                repository.IsExistsAsync(x => x.CategoryName == inputCategory.CategoryName))
+                    .ReturnsAsync(true);
+
+            // when
+            Task addtCategoryByIdTask = _categoryService
+                .AddAsync(inputCategory);
+
+            // then
+            _categoryRepositoryMock.Verify(repository =>
+                repository.IsExistsAsync(It.IsAny<Expression<Func<Category, bool>>>()),
+                    Times.Once);
+
+            await Assert.ThrowsAsync<DuplicationException>(() =>
+                addtCategoryByIdTask);
 
             _categoryRepositoryMock.VerifyNoOtherCalls();
             _categoryUnitOfWorkMock.VerifyNoOtherCalls();
