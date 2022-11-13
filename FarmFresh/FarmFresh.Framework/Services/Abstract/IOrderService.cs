e19b9a3135f54def2f3a170ac7c63695e6fd3b7d
Talat Tanbir <hossaintanbir0@gmail.com>
2022-11-13 11:33:46 +0600

    Add GetOrders Service Methods

--- FarmFresh/FarmFresh.Framework/Services/Abstract/IOrderService.cs
+++ FarmFresh/FarmFresh.Framework/Services/Abstract/IOrderService.cs
@@ -1,6 +1,13 @@
-﻿namespace FarmFresh.Framework.Services.Abstract
+﻿using FarmFresh.Framework.Entities.Orders;
+
+namespace FarmFresh.Framework.Services.Abstract
 {
     public interface IOrderService : IDisposable
     {
+        Task<IEnumerable<OrderItem>> GetOrderItemsAsync(int orderId);
+
+        Task<CustomerOrder> GetCustomerOrderAsync(string customerEmail, int orderId);
+
+        Task<IEnumerable<Order>> GetCustomerOrdersAsync(string customerEmail);
     }
 }
--- FarmFresh/FarmFresh.Framework/Services/Concrete/OrderService.cs
+++ FarmFresh/FarmFresh.Framework/Services/Concrete/OrderService.cs
@@ -1,4 +1,7 @@
-﻿using FarmFresh.Framework.Services.Abstract;
+﻿using FarmFresh.Common.Exceptions;
+using FarmFresh.Framework.Entities.Orders;
+using FarmFresh.Framework.Entities.Users;
+using FarmFresh.Framework.Services.Abstract;
 using FarmFresh.Framework.UnitOfWorks.Abstract;
 
 namespace FarmFresh.Framework.Services.Concrete
@@ -22,6 +25,49 @@ namespace FarmFresh.Framework.Services.Concrete
             _customerOrderUnitOfWork = customerOrderUnitOfWork;
             _userService = userService;
             _cartService = cartService;
+        }
+
+        public async Task<IEnumerable<OrderItem>> GetOrderItemsAsync(int orderId)
+        {
+            var order = await _orderUnitOfWork.OrderRepository.GetByIdAsync(orderId);
+            var orderItems = order.OrderItems.AsEnumerable();
+
+            return orderItems;
+        }
+
+        public async Task<CustomerOrder> GetCustomerOrderAsync(string customerEmail, int orderId)
+        {
+            var customer = await _userService.GetAsync(customerEmail);
+
+            if (customer is null)
+            {
+                throw new NotFoundException(nameof(User), nameof(customerEmail));
+            }
+
+            var customerOrder = await _customerOrderUnitOfWork.CustomerOrderRepository.GetFirstOrDefaultAsync<CustomerOrder>(
+                x => x,
+                x => x.CustomerId == customer.Id && x.OrderId == orderId);
+
+            return customerOrder;
+        }
+
+        public async Task<IEnumerable<Order>> GetCustomerOrdersAsync(string customerEmail)
+        {
+            var customer = await _userService.GetAsync(customerEmail);
+
+            if (customer is null)
+            {
+                throw new NotFoundException(nameof(User), nameof(customerEmail));
+            }
+
+            var customerOrders = await _customerOrderUnitOfWork.CustomerOrderRepository.GetAsync(
+                x => x.Order,
+                x => x.CustomerId == customer.Id,
+                x => x.OrderBy(o => o.Order.Created),
+                null,
+                true);
+
+            return customerOrders;
         }
 
         public void Dispose()
