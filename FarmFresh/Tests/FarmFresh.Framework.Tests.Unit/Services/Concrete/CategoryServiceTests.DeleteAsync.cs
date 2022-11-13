2260d271074f404cddc5c925834cee202592abc6
Talat Tanbir <hossaintanbir0@gmail.com>
2022-11-13 18:35:24 +0600

    PASSED -> DeleteAsync_NoErrorOccurs_ShouldDeleteCategory()

--- FarmFresh/Tests/FarmFresh.Framework.Tests.Unit/Services/Concrete/CategoryServiceTests.DeleteAsync.cs
+++ FarmFresh/Tests/FarmFresh.Framework.Tests.Unit/Services/Concrete/CategoryServiceTests.DeleteAsync.cs
@@ -1,12 +1,41 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using FarmFresh.Framework.Entities.Categories;
+using Moq;
+using System.Linq.Expressions;
+using Xunit;
 
 namespace FarmFresh.Framework.Tests.Unit.Services.Concrete
 {
-    public class CategoryServiceTests
+    public partial class CategoryServiceTests
     {
+        [Fact]
+        public async Task DeleteAsync_NoErrorOccurs_ShouldDeleteCategory()
+        {
+            // given
+            int randomCategoryId = GetRandomNumber();
+            int inputCategoryId = randomCategoryId;
+            Category randomCategory = GetRandomCategory();
+
+            _categoryUnitOfWorkMock.Setup(x => x.CategoryRepository).Returns(_categoryRepositoryMock.Object);
+
+            _categoryRepositoryMock.Setup(repository =>
+                repository.DeleteAsync(x => x.Id == inputCategoryId))
+                    .Verifiable();
+
+            // when
+            await _categoryService
+                .DeleteAsync(inputCategoryId);
+
+            // then
+            _categoryRepositoryMock.Verify(repository =>
+                repository.DeleteAsync(It.IsAny<Expression<Func<Category, bool>>>()),
+                    Times.Once);
+
+            _categoryUnitOfWorkMock.Verify(repository =>
+                repository.SaveChangesAsync(),
+                    Times.Once);
+
+            _categoryRepositoryMock.VerifyNoOtherCalls();
+            _categoryUnitOfWorkMock.VerifyNoOtherCalls();
+        }
     }
 }
